d172517781ef165a2ad05d2f2270b17a2144febb
niggy1813@gmx.net <niggy1813@gmx.net@fb0a1176-3945-11de-a9fe-ad1e22fc3a8a>
2009-05-07 20:39:28 +0000

    KI fast fertig (hat nur noch kleine Fehler, kommen aber bis spätestens Sonntag weg)

--- trunk/src/org/mss/games/Chomp.java
+++ trunk/src/org/mss/games/Chomp.java
@@ -100,8 +100,6 @@ public class Chomp extends Spiel {
 
 	private Turn kI(Spieler spieler) {
 		
-		System.out.println(width + " " + height);
-		
 		boolean needKi = false;
 		boolean isFree = false;
 		int col = 0;
@@ -114,7 +112,7 @@ public class Chomp extends Spiel {
 		
 		// prüfen, ob in den ersten beiden Reihen schon was gesetzt ist
 		for(int i = 0; i < 2; i++){
-			for(int j = 0; j < feld.length; j++){
+			for(int j = 0; j < width; j++){
 				if(feld[i][j] != " "){
 					needKi = true;
 				}
@@ -122,7 +120,7 @@ public class Chomp extends Spiel {
 		}
 		
 		// prüfen, ob in den ersten beiden Spalten schon was gesetzt ist
-		for(int i = feld.length; i < 2; i++){
+		for(int i = height; i < 2; i++){
 			for(int j = 0; j < 2; j++){
 				if(feld[i][j] != " "){
 					needKi = true;
@@ -146,8 +144,71 @@ public class Chomp extends Spiel {
 			}
 		} 
 		// ab hier greift KI
-		else { 
-			System.out.println("Hier brauchen wir KI^^");
+		else { 	
+			
+			// wenn in 2. Reihe ein Feld besetzt, besetze 2. Reihe außer die beiden linken Felder 
+			if(feld[1][2].equals(" ")){
+				for(int j = 2; j < feld.length; j++){
+					if(feld[1][j] != " "){
+						System.out.println("oben rechts");
+						col = 2;
+						row = 8;
+						break;
+					}
+				}
+			}				
+
+			// wenn in 2. Spalte ein Feld besetzt, besetze 2 Spalte, bis auf das oberste Feld
+			if(feld[2][1].equals(" ")){
+				for(int j = 1; j < 2; j++){
+					if(feld[j][2] != " "){
+						System.out.println("unten links");
+						col = 1;
+						row = 8;
+						break;
+					}
+				}
+			}
+			
+			// wenn feld[1][1] besetzt, markiere linkes unterstes Feld
+			if(!feld[1][1].equals(" ")){
+				col = 0;
+				row = 0;
+			}
+			
+			
+			int set = 0;
+			int direction = 0; // 0 = horizontal; 1 = vertical;
+			
+			for(int i = 0; i < width; i++){
+				if(!feld[i][0].equals(" ")){
+					set = i;
+					break;
+				}
+			}
+
+			System.out.println("set 1: " + set);
+			for(int i = 0; i < height; i++){
+				if(!feld[0][i].equals(" ")){
+					if(i < set){
+						set = i;
+						direction = 1;
+						break;
+					}
+				}
+			}
+			
+			if(set != 0){
+				if(direction == 0){
+					col = set;
+					row = width-1;
+				} else {
+					col = 0;
+					row = height - 1 - set;
+				}
+			}
+				
+			
 		}
 		
 
