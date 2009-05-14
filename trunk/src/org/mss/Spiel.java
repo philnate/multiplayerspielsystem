bd94e8988bf19dc9b15ce6b2e46e5918f58ac435
philnate <philnate@fb0a1176-3945-11de-a9fe-ad1e22fc3a8a>
2009-05-14 07:23:04 +0000

    Refractoring, Vars alle auf Deutsch.

--- trunk/src/org/mss/Spiel.java
+++ trunk/src/org/mss/Spiel.java
@@ -1,7 +1,7 @@
 package org.mss;
 
 import org.mss.Spieler;
-import org.mss.types.Turn;
+import org.mss.types.Zug;
 import org.mss.utils.Console;
 
 import java.util.ArrayList;
@@ -10,20 +10,20 @@ import java.util.Iterator;
 public abstract class Spiel extends Spielfeld implements Protokollierbar {
 	protected ArrayList<Spieler> spieler = null;
 	protected boolean track = false;
-	protected Spieler[] winner = null;
-	public abstract Turn queryPlayer(Spieler spieler);
-	public abstract Spieler[] spielzug(Turn turn) throws Exception;
+	protected Spieler[] gewinner = null;
+	public abstract Zug frageSpieler(Spieler spieler);
+	public abstract Spieler[] spielzug(Zug turn) throws Exception;
 	public abstract Spieler[] runde();
-	public abstract Spieler nextPlayer();
+	public abstract Spieler folgeSpieler();
 
 	public Spieler[] durchgang() {
 		boolean error =false;
 		do {
 			error = false;
 			try {
-				spielzug(queryPlayer(nextPlayer()));
-				if (winner != null) {
-					return winner;
+				spielzug(frageSpieler(folgeSpieler()));
+				if (gewinner != null) {
+					return gewinner;
 				}
 			} catch (Exception e) {
 				error = true;
@@ -34,18 +34,18 @@ public abstract class Spiel extends Spielfeld implements Protokollierbar {
 		do {
 			error = false;
 			try {
-				spielzug(queryPlayer(nextPlayer()));
-				return winner;
+				spielzug(frageSpieler(folgeSpieler()));
+				return gewinner;
 			} catch (Exception e) {
 				error = true;
 				e.printStackTrace();
 				Console.read("Wait a second", 42);
 			}
 		} while (error);
-		return winner;
+		return gewinner;
 	}
 
-	public Spieler[] playerList() {
+	public Spieler[] listeSpieler() {
 		Iterator<Spieler> it = this.spieler.iterator();
 		Spieler[] spieler = new Spieler[this.spieler.size()];
 		int i = 0;
@@ -63,27 +63,27 @@ public abstract class Spiel extends Spielfeld implements Protokollierbar {
 		this.track = track;
 	}
 	
-	public void addTurn(Turn turn) {
-		System.out.println(turn.toString());
-		turns.add(turn);
+	public void speicherZug(Zug zug) {
+		System.out.println(zug.toString());
+		zuege.add(zug);
 	}
 
-	public int findTurn(Turn turn) {
-		Turn temp = null;
-		Iterator<Turn> it = turns.iterator();
+	public int findeZug(Zug zug) {
+		Zug temp = null;
+		Iterator<Zug> it = zuege.iterator();
 
 		while (it.hasNext()) {
-			temp = (Turn) it.next();
-			if (temp.equals(turn)) {
-				return turns.size() - turns.indexOf(temp);//So das der Spieler weiß der wie vielte Zug es war;
+			temp = (Zug) it.next();
+			if (temp.equals(zug)) {
+				return zuege.size() - zuege.indexOf(temp);//So das der Spieler weiß der wie vielte Zug es war;
 			}
 		}
 		return -1;
 	}
 
-	public void listTurns() {
-		Iterator<Turn> iterate = turns.iterator();
-		Turn turn = null;
+	public void listeZuege() {
+		Iterator<Zug> iterate = zuege.iterator();
+		Zug turn = null;
 		System.out.println("Spielzüge:");
 		while (iterate.hasNext()) {
 			turn = iterate.next();
@@ -91,27 +91,27 @@ public abstract class Spiel extends Spielfeld implements Protokollierbar {
 		}
 	}
 
-	public void displayFeld() {
-		for (int i = 0; i < height; i++) {
+	public void zeigeFeld() {
+		for (int i = 0; i < hoehe; i++) {
 			System.out.print("|");
-			for (int j = 0; j < width; j++) {
+			for (int j = 0; j < breite; j++) {
 				System.out.print("--|");
 			}
 			System.out.println();
 			System.out.print("|");
-			for (int j = 0; j < width; j++) {
+			for (int j = 0; j < breite; j++) {
 				System.out.print(feld[i][j]+ " |");
 			}
-			System.out.println(height-i-1);
+			System.out.println(hoehe-i-1);
 		}
 		System.out.print("|");
-		for (int j = 0; j < width; j++) {
+		for (int j = 0; j < breite; j++) {
 			System.out.print((Integer.toString(j).length() ==1)? j+"-|": j+"|");
 		}
 		System.out.println();
 	}
 	
-	public boolean addPlayer(Spieler spieler) throws Exception{
+	public boolean plusSpieler(Spieler spieler) throws Exception{
 		return this.spieler.add(spieler);
 	}
 }
