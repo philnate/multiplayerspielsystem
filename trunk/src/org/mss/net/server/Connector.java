b22f943f58b1de6a1fcb0ce7e44ed468fc5eccb1
Philnate <Philnate@fb0a1176-3945-11de-a9fe-ad1e22fc3a8a>
2009-06-03 16:17:43 +0000

    Comments und fehlende Synchronized Blöcke, Verbieten von Nachrichten senden ohne gültige Anmeldung.

--- trunk/src/org/mss/net/server/Connector.java
+++ trunk/src/org/mss/net/server/Connector.java
@@ -2,30 +2,33 @@ package org.mss.net.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+
 import org.mss.utils.Console;
 
-public class Connector extends Thread
-{
-  private ServerSocket listener = null;
+/*
+ * Klasse Connector übernimmt die Rolle des Verbinders
+ */
+public class Connector extends Thread {
+	private ServerSocket listener = null;
 
-  public void run()
-  {
-    Console.log("Warte auf neuen Teilnehmer!");
-    try
-    {
-      while (true) {
-      ClientThread client = new ClientThread(this.listener.accept());
-      Thread clientThread = new Thread(client);
-      clientThread.start();
-      }
-    } catch (IOException e) {
-      Console.log("Fehler bei Verbindungsaufnahme mit Client!");
-      e.printStackTrace();
-    }
-  }
+	public void run() {
+		Console.log("Warte auf neuen Teilnehmer!");
+		try {
+			while (true) {
+				//wartet auf neuen Teilnehmer und weist ihm anschließend seinen ClientThread zu
+				ClientThread client = new ClientThread(this.listener.accept());
+				Thread clientThread = new Thread(client);
+				clientThread.start();
+			}
+		} catch (IOException e) {
+			//Irgendein Fehler ist aufgetreten im Socket, lieber beenden.
+			Console.log("Fehler bei Verbindungsaufnahme mit Client!");
+			System.exit(1);
+			e.printStackTrace();
+		}
+	}
 
-  Connector(ServerSocket listener)
-  {
-    this.listener = listener;
-  }
+	Connector(ServerSocket listener) {
+		this.listener = listener;
+	}
 }
