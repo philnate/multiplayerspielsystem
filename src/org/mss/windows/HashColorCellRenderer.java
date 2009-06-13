0f497f5b9f3d66f6b2f439392483dd22c182ea48
Philnate <Philnate@fb0a1176-3945-11de-a9fe-ad1e22fc3a8a>
2009-06-13 22:38:05 +0000

    Benutzer werden je nach Hashwert des Namens farbig dargestellt, alle Namen die admin enthalten sind gesperrt und werden mit LOGIN_ALREADY_ON quittiert.

---
+++ src/org/mss/windows/HashColorCellRenderer.java
@@ -0,0 +1,23 @@
+package org.mss.windows;
+
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+class HashColorCellRenderer extends JLabel implements ListCellRenderer {
+	private static final long serialVersionUID = 3951805205071348113L;
+
+	public HashColorCellRenderer() {
+        setOpaque(true);
+    }
+
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+        setText(value.toString());
+        setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
+        setForeground(new Color(value.toString().hashCode(), false));
+        return this;
+    }
+}
